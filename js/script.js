4814654cecf97666ba68450eb4fe9eaaa6de0674
Kurt Leadley <kleadley@my365.bellevue.edu>
2019-08-16 00:41:18 -0400

    push cmg

--- js/script.js
+++ js/script.js
@@ -1,4 +1,8 @@
 $( document ).ready(function() {
     console.log( "ready!" );
-    $('#postheader2-tech-middle > h3').css('visibility','hidden')
+    $('#postheader2-tech-middle > h3').css('visibility','hidden');
+    var height = $('#postheader2-right').height();
+    console.log(height);
+    $('#postheader2-left').height(height);
+    $('#postheader2-right').height(height);
 });
