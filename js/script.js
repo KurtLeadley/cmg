4b3b56fc7c1c810c04ef0aab2e34d5cdb84526e8
Kurt Leadley <kleadley@my365.bellevue.edu>
2019-08-21 22:34:09 -0400

    update

--- js/script.js
+++ js/script.js
@@ -1,8 +1,25 @@
 $( document ).ready(function() {
-    console.log( "ready!" );
     $('#postheader2-tech-middle > h3').css('visibility','hidden');
-    var height = $('#postheader2-right').height();
-    console.log(height);
-    $('#postheader2-left').height(height);
-    $('#postheader2-right').height(height);
+    $('#menu-btn').css('visibility','hidden');
+    console.log('hi?');
+    if($(window).width() >= 1024){
+        adjustHeight('#postheader2-left','#postheader2-right');
+        adjustHeight('#postheader2-left','#postheader2-right');
+    }
+    var url = window.location.href;
+    $("nav a").each(function() {
+        if (url == (this.href)) {
+            $(this).closest("li a").addClass("active");
+            //for making parent of submenu active
+            $(this).closest("li a").parent().parent().addClass("active");
+        }
+    });
 });
+function adjustHeight(el1,el2) {
+    var elementTarget = el1;
+    var elementToChange = el2;
+    var targetHeight = $(elementTarget).height();
+    $(elementToChange).height(targetHeight);
+    $(elementTarget).height(targetHeight);
+    console.log(targetHeight);
+}
